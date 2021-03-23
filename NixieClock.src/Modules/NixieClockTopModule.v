4886404d6b7a7e6b2c6bb8bfdfbddd412e726b61
BlackIsDevin <58799343+BlackIsDevin@users.noreply.github.com>
2021-03-23 02:36:31 -0400

    added NixieClockTopModule

---
+++ NixieClock.src/Modules/NixieClockTopModule.v
@@ -0,0 +1,10 @@
+////////////////////////////////////////////////////////////////////////////////
+// File: NixieClockTopModule.v
+// Author: BlackIsDevin (https://github.com/BlackIsDevin)
+// Creation Date: 3/23/2021‏‎
+// Target Devices: Mimas A7 Revision V3 Development Board 
+//
+// Description: This module links together all the underlying modules for the
+// NixieClock project and exposes inputs and outputs for the FPGA and any
+// testbench modules.
+////////////////////////////////////////////////////////////////////////////////
