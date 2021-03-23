1d73d10f238570f6a55385cca8a9781ca89fea61
BlackIsDevin <58799343+BlackIsDevin@users.noreply.github.com>
2021-03-23 02:37:40 -0400

    added InputHandler

---
+++ NixieClock.src/Modules/InputHandler.v
@@ -0,0 +1,9 @@
+////////////////////////////////////////////////////////////////////////////////
+// File: InputHandler.v
+// Author: BlackIsDevin (https://github.com/BlackIsDevin)
+// Creation Date: 3/23/2021‏‎
+// Target Devices: Mimas A7 Revision V3 Development Board 
+//
+// Description: This module parses the user input (buttons and dip switches) 
+// and generates signals for the ClockStateStorage module to interpret.
+////////////////////////////////////////////////////////////////////////////////
