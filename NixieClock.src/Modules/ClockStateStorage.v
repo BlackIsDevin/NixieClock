6b5c7416079758e28c859e2569ea3084d1249c9e
BlackIsDevin <58799343+BlackIsDevin@users.noreply.github.com>
2021-03-23 02:37:30 -0400

    added ClockStateStorage

---
+++ NixieClock.src/Modules/ClockStateStorage.v
@@ -0,0 +1,11 @@
+////////////////////////////////////////////////////////////////////////////////
+// File: ClockStateStorage.v
+// Author: BlackIsDevin (https://github.com/BlackIsDevin)
+// Creation Date: 3/23/2021‏‎
+// Target Devices: Mimas A7 Revision V3 Development Board 
+//
+// Description: This module holds the current value of the clock as well as
+// manages incrementing the clock every second. This module also can accept
+// signals from the InputHandler module to increment the three different
+// segments of the clock.
+////////////////////////////////////////////////////////////////////////////////
