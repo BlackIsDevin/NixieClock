9254825e10ba14f6e3f78b08cb3acf50c0fdecce
BlackIsDevin <58799343+BlackIsDevin@users.noreply.github.com>
2021-03-23 02:36:55 -0400

    added NixieSignalGeneration

---
+++ NixieClock.src/Modules/NixieSignalGeneration.v
@@ -0,0 +1,9 @@
+////////////////////////////////////////////////////////////////////////////////
+// File: NixieSignalGeneration.v
+// Author: BlackIsDevin (https://github.com/BlackIsDevin)
+// Creation Date: 3/23/2021‏‎
+// Target Devices: Mimas A7 Revision V3 Development Board 
+//
+// Description: This module takes in the value of the clock state, and generates
+// the appropriate signals to drive the nixie tubes.
+////////////////////////////////////////////////////////////////////////////////
