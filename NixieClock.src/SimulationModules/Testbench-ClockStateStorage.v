609c56cf783c527e7def5368503578de297acf60
BlackIsDevin <58799343+BlackIsDevin@users.noreply.github.com>
2021-03-27 16:18:22 -0400

    added testbench for clockstatestorage module

---
+++ NixieClock.src/SimulationModules/Testbench-ClockStateStorage.v
@@ -0,0 +1,15 @@
+////////////////////////////////////////////////////////////////////////////////
+// File: Testbench-ClockStateStorage.v
+// Author: BlackIsDevin (https://github.com/BlackIsDevin)
+// Creation Date: 3/27/2021‏‎
+// Target Devices: Mimas A7 Revision V3 Development Board 
+//
+// Description: This module is intended to test and verify the functionality of
+// the ClockStateStorage module.
+////////////////////////////////////////////////////////////////////////////////
+
+module TestbenchClockStateStorage(
+
+);
+
+endmodule
